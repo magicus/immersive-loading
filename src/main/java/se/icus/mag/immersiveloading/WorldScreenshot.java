33a981334128dc98fc7d429d631e9031748edd5f
Magnus Ihse Bursie <mag@icus.se>
2026-09-19 20:27:15 +0200

    chore: Run spotless

--- src/main/java/se/icus/mag/immersiveloading/WorldScreenshot.java
+++ src/main/java/se/icus/mag/immersiveloading/WorldScreenshot.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright © Magnus Ihse Bursie 2026.
+ * This file is released under LGPLv3. See LICENSE for full license details.
+ */
 package se.icus.mag.immersiveloading;
 
 import com.mojang.blaze3d.platform.NativeImage;
